ef764c9f7ac0d5650e0c0b217fb1630f05a9553b
Kyler Stratton <59697829+KylerStr@users.noreply.github.com>
2024-06-05 22:47:38 -0500

    Project Opening - Scene Creation - General Module
    - Implement ability to open an existing project (Still needs to prompt to save for if a project is already open)
    - Implement ability to create a new scene (very basic level)
    - Implement ability to parse scenes folder and build into a list
    - Implemented tabs to main screen of engine

---
+++ Modules/EngineModules/PSTRPG-GeneralFunctions.ps1
@@ -0,0 +1,48 @@
+﻿#Generic Functions Module
+function New-GUID {
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		[ValidateSet('Scene', 'View', 'Item', 'Event', 'NPC', IgnoreCase = $true)]
+		[String]$GuidType
+	)
+	$GeneratedGUID = ""
+	$GuidTypeID = ''
+	$GuidRandomString = ''
+	switch ($GuidType) {
+		'Scene' {
+			$GuidTypeID = '5cf43ed8'
+		}
+		'View' {
+			$GuidTypeID = '769dea12'
+		}
+		'Item' {
+			$GuidTypeID = '77e5ad0e'
+		}
+		'Event' {
+			$GuidTypeID = '3151c39f'
+		}
+		'NPC' {
+			$GuidTypeID = '4cd09cb6'
+		}
+	}
+	#Generate rest of GUID
+	For ($i = 0; $i -lt 28; $i++) {
+		If ($i -eq 0 -or $i -eq 5 -or $i -eq 10) {
+			$GuidRandomString = $GuidRandomString + '-'
+		}
+		Else {
+			$D4 = Get-Random -Minimum 1 -Maximum 4
+			if ($CoinFlip -eq 1) {
+				#97.122 - Letters
+				$GuidRandomString = $GuidRandomString + "$([char](Get-Random -Minimum 97 -Maximum 122))"
+			}
+			Else {
+				$GuidRandomString = $GuidRandomString + "$(Get-Random -Minimum 0 -Maximum 9)"
+			}
+		}
+		
+	}
+	$GeneratedGUID = "$GuidTYPEID" + "$GuidRandomString"
+	return $GeneratedGUID
+}
